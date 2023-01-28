c7653185bcbd7d77e0f35cf045076992177c9ab9
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-01-29 01:44:20 +0900

    Add NoArgsConstructor

--- src/main/java/com/jwj/community/domain/entity/member/auth/embedded/MemberRolesId.java
+++ src/main/java/com/jwj/community/domain/entity/member/auth/embedded/MemberRolesId.java
@@ -5,7 +5,11 @@ import com.jwj.community.domain.entity.member.auth.Role;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+// 엔티티에 포함되는 embedded 객체도 기본생성자 필요
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberRolesId {
 
     @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/com/jwj/community/domain/entity/member/embedded/BirthDay.java
+++ src/main/java/com/jwj/community/domain/entity/member/embedded/BirthDay.java
@@ -1,7 +1,12 @@
 package com.jwj.community.domain.entity.member.embedded;
 
 import jakarta.persistence.Column;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+// 엔티티에 포함되는 embedded 객체도 기본생성자 필요
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BirthDay {
 
     @Column(name = "birth_year", columnDefinition = "smallint")
@@ -13,4 +18,10 @@ public class BirthDay {
     @Column(name = "birth_day", columnDefinition = "smallint")
     private Integer day;
 
+    @Builder
+    public BirthDay(Integer year, Integer month, Integer day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
 }
