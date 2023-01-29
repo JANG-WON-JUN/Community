c84e0ab7815bf85d277647b18466c34bf7f75120
w32807 <w32807@nate.com>
2023-01-29 20:22:13 +0900

    Add result dto for response to client

---
+++ src/main/java/com/jwj/community/web/common/result/ListResult.java
@@ -0,0 +1,26 @@
+package com.jwj.community.web.common.result;
+
+import com.jwj.community.web.common.paging.PageInfo;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+import static com.jwj.community.web.common.paging.PageInfo.of;
+
+
+@Data
+public class ListResult<T> {
+
+    private final List<T> list;
+    private final int size;
+    private final PageInfo pageInfo;
+
+    @Builder
+    public ListResult(List<T> list, Page page) {
+        this.list = list;
+        this.size = list.size();
+        this.pageInfo = of(page);
+    }
+}
