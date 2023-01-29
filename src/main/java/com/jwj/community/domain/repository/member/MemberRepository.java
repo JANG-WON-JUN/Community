3a934b120190966c46f02b677b9eb031f7061b49
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-01-29 23:21:16 +0900

    Modify return value of findByEmail method Member Entity to Optional

--- src/main/java/com/jwj/community/domain/repository/member/MemberRepository.java
+++ src/main/java/com/jwj/community/domain/repository/member/MemberRepository.java
@@ -3,7 +3,9 @@ package com.jwj.community.domain.repository.member;
 import com.jwj.community.domain.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Member findByEmail(String email);
+    Optional<Member> findByEmail(String email);
 }
