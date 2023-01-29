3fb491be16d20b9e645b060d8152d25ac44ce92d
w32807 <w32807@nate.com>
2023-01-29 11:41:22 +0900

    add JwtAuthenticationFailureHandler

---
+++ src/main/java/com/jwj/community/config/security/handler/JwtAuthenticationFailureHandler.java
@@ -0,0 +1,66 @@
+package com.jwj.community.config.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jwj.community.web.exception.dto.ErrorResult;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Locale.getDefault;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    private final MessageSource messageSource;
+    private final ObjectMapper mapper;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response
+            , AuthenticationException exception) throws IOException {
+        ErrorResult errorResult = ErrorResult.builder()
+                .errorCode(UNAUTHORIZED)
+                .errorMessage(getErrorMessage(exception))
+                .exception(exception)
+                .build();
+
+        response.setStatus(UNAUTHORIZED.value());
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(UTF_8.name());
+
+        mapper.writeValue(response.getWriter(), errorResult);
+    }
+
+    private String getErrorMessage(AuthenticationException exception) {
+        String errorMessage = messageSource.getMessage("confirm.emailOrPassword", null, getDefault());
+
+        if(exception instanceof UsernameNotFoundException){
+            errorMessage = messageSource.getMessage("confirm.email", null, getDefault());
+        }
+
+        if(exception instanceof BadCredentialsException){
+            errorMessage = messageSource.getMessage("confirm.pwd", null, getDefault());
+        }
+
+        if(exception instanceof InsufficientAuthenticationException){
+            errorMessage = messageSource.getMessage("error.extraAuth", null, getDefault());
+        }
+
+        if(exception instanceof AuthenticationServiceException){
+            errorMessage = exception.getMessage();
+        }
+
+        return errorMessage;
+    }
+}
