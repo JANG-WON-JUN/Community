dc4ba5b2223a43b3f071673006f65e3d1babcfa3
w32807 <w32807@nate.com>
2023-01-29 11:27:08 +0900

    add common error result dto

---
+++ src/main/java/com/jwj/community/web/exception/dto/ErrorResult.java
@@ -0,0 +1,23 @@
+package com.jwj.community.web.exception.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+public class ErrorResult {
+
+    private final String errorCode;
+    private final String errorMessage;
+    private final Exception exception;
+    private final String exceptionName;
+
+    @Builder
+    public ErrorResult(HttpStatus errorCode, String errorMessage, Exception exception){
+        this.errorCode = String.valueOf(errorCode.value());
+        this.errorMessage = errorMessage;
+        this.exception = exception;
+        this.exceptionName = exception.getClass().getSimpleName();
+    }
+
+}
