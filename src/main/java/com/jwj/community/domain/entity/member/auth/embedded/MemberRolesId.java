6997e3c796f5cfb08e228b19599bed9eb7c517a1
w32807 <w32807@nate.com>
2023-02-01 08:33:29 +0900

    modify mapping logic with MemberRolesId entity

--- src/main/java/com/jwj/community/domain/entity/member/auth/embedded/MemberRolesId.java
+++ src/main/java/com/jwj/community/domain/entity/member/auth/embedded/MemberRolesId.java
@@ -5,16 +5,14 @@ import com.jwj.community.domain.entity.member.auth.Role;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 // 엔티티에 포함되는 embedded 객체도 기본생성자 필요
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberRolesId {
 
+    @Setter
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", referencedColumnName = "id")
     private Member member;
