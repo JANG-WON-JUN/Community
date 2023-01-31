8a551e8f70403bc5258d3b525185aaa49c5e220b
w32807 <w32807@nate.com>
2023-01-31 11:14:27 +0900

    modify package of RefreshTokenRepository

--- src/main/java/com/jwj/community/domain/repository/member/RefreshTokenRepository.java
+++ src/main/java/com/jwj/community/domain/repository/member/RefreshTokenRepository.java
@@ -1,4 +1,4 @@
-package com.jwj.community.domain.repository;
+package com.jwj.community.domain.repository.member;
 
 import com.jwj.community.domain.entity.member.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
