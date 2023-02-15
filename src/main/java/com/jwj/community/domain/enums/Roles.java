59ea0668f92aea8fb440bc3d13fd8537405aa106
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-02-15 14:48:22 +0900

    modify ROLE_USER to ROLE_ANONYMOUS

--- src/main/java/com/jwj/community/domain/enums/Roles.java
+++ src/main/java/com/jwj/community/domain/enums/Roles.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 public enum Roles {
     // Spring Security에서는 권한을 나타내는 문자열의 접두어에 기본으로 ROLE_를 붙여주므로
     // 동일하게 Roles enum을 작성함
-    ROLE_USER("비회원"),
+    ROLE_ANONYMOUS("비회원"),
     ROLE_MEMBER("회원"),
     ROLE_ADMIN("관리자");
 
@@ -20,6 +20,6 @@ public enum Roles {
         return Arrays.stream(Roles.values())
                 .filter(role -> role.name().equals(roleName))
                 .findFirst()
-                .orElse(ROLE_USER);
+                .orElse(ROLE_ANONYMOUS);
     }
 }
