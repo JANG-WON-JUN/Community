9e8d3a711143dfcbb3818ec6818a9d9af5024345
w32807 <w32807@nate.com>
2023-02-15 14:34:41 +0900

    add findByResourceType method

---
+++ src/main/java/com/jwj/community/domain/repository/member/auth/ResourcesQueryRepository.java
@@ -0,0 +1,13 @@
+package com.jwj.community.domain.repository.member.auth;
+
+
+import com.jwj.community.domain.entity.member.auth.Resources;
+import com.jwj.community.domain.enums.ResourceType;
+
+import java.util.List;
+
+public interface ResourcesQueryRepository {
+
+    List<Resources> findByResourceType(ResourceType resourceType);
+
+}
--- src/main/java/com/jwj/community/domain/repository/member/auth/ResourcesRepository.java
+++ src/main/java/com/jwj/community/domain/repository/member/auth/ResourcesRepository.java
@@ -3,5 +3,5 @@ package com.jwj.community.domain.repository.member.auth;
 import com.jwj.community.domain.entity.member.auth.Resources;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ResourcesRepository extends JpaRepository<Resources, Long> {
+public interface ResourcesRepository extends JpaRepository<Resources, Long>, ResourcesQueryRepository {
 }
