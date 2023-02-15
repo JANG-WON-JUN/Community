e67e0de6bc161e6a50cf66ffedb2c305fe00aead
w32807 <w32807@nate.com>
2023-02-15 16:43:22 +0900

    add findByResourceType method

--- src/main/java/com/jwj/community/domain/service/member/auth/ResourcesService.java
+++ src/main/java/com/jwj/community/domain/service/member/auth/ResourcesService.java
@@ -1,9 +1,12 @@
 package com.jwj.community.domain.service.member.auth;
 
 import com.jwj.community.domain.entity.member.auth.Resources;
+import com.jwj.community.domain.enums.ResourceType;
 import com.jwj.community.domain.repository.member.auth.ResourcesRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +16,9 @@ public class ResourcesService {
 
     public void createResources(Resources resources){
         resourcesRepository.save(resources);
+    }
+
+    public List<Resources> findByResourceType(ResourceType resourceType){
+        return resourcesRepository.findByResourceType(resourceType);
     }
 }
