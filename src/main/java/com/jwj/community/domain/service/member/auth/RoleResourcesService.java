1eee31780d3c00676bde6b9ea0cc31b8dc1f7deb
w32807 <w32807@nate.com>
2023-02-15 16:44:50 +0900

    add RoleResourcesService and test class

---
+++ src/main/java/com/jwj/community/domain/service/member/auth/RoleResourcesService.java
@@ -0,0 +1,26 @@
+package com.jwj.community.domain.service.member.auth;
+
+import com.jwj.community.domain.entity.member.auth.RoleResources;
+import com.jwj.community.domain.enums.Roles;
+import com.jwj.community.domain.repository.member.auth.RoleResourcesRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class RoleResourcesService {
+
+    private final RoleResourcesRepository roleResourcesRepository;
+
+    public void createRoleResources(RoleResources roleResources) {
+        roleResourcesRepository.save(roleResources);
+    }
+
+    public List<RoleResources> findByRoleName(Roles role){
+        return roleResourcesRepository.findByIdRoleRoleName(role);
+    }
+}
