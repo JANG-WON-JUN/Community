5757c09938be59ad132b920c470a39a608fc9319
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-02-15 16:43:59 +0900

    modify Fetch lazy to eager

--- src/main/java/com/jwj/community/domain/entity/member/auth/embedded/RoleResourcesId.java
+++ src/main/java/com/jwj/community/domain/entity/member/auth/embedded/RoleResourcesId.java
@@ -6,6 +6,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,12 +16,17 @@ import lombok.NoArgsConstructor;
 public class RoleResourcesId {
 
     // 복합키에서는 엔티티의 PK 컬럼명이 id더라도 변수명 + id로 joincolumn에 엔티티클래스명 + id로 명시해줘야 한다.
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "role_id")
     private Role role;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "resources_id")
     private Resources resources;
 
+    @Builder
+    public RoleResourcesId(Role role, Resources resources) {
+        this.role = role;
+        this.resources = resources;
+    }
 }
