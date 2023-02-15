37c2a81124a0da811f1a56e932d1882804579ce9
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-02-15 14:24:39 +0900

    add save basic resources insert logic

--- src/main/java/com/jwj/community/domain/service/member/auth/ResourcesService.java
+++ src/main/java/com/jwj/community/domain/service/member/auth/ResourcesService.java
@@ -1,5 +1,6 @@
 package com.jwj.community.domain.service.member.auth;
 
+import com.jwj.community.domain.entity.member.auth.Resources;
 import com.jwj.community.domain.repository.member.auth.ResourcesRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,7 @@ public class ResourcesService {
 
     private final ResourcesRepository resourcesRepository;
 
-    public void createResources(){
-
+    public void createResources(Resources resources){
+        resourcesRepository.save(resources);
     }
 }
