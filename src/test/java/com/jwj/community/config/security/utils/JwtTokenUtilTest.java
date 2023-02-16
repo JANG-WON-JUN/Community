a24335a28083473e39aa823b8254922a603e5723
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-02-16 12:41:44 +0900

    add @ActiveProfiles("test") annotation

--- src/test/java/com/jwj/community/config/security/utils/JwtTokenUtilTest.java
+++ src/test/java/com/jwj/community/config/security/utils/JwtTokenUtilTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Set;
 
@@ -17,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
+@ActiveProfiles("test") // test 시에는 test profile을 사용할 수 있도록 설정
 class JwtTokenUtilTest {
 
     @Autowired
