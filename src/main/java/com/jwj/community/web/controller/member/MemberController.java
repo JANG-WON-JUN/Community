fe2388b25f03e71379016c85f99a10e80cf81842
w32807 <w32807@nate.com>
2023-02-17 10:11:00 +0900

    add @Valid annotation

--- src/main/java/com/jwj/community/web/controller/member/MemberController.java
+++ src/main/java/com/jwj/community/web/controller/member/MemberController.java
@@ -2,6 +2,7 @@ package com.jwj.community.web.controller.member;
 
 import com.jwj.community.domain.service.member.MemberService;
 import com.jwj.community.web.dto.member.request.MemberCreate;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +18,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/api/join")
-    public ResponseEntity<String> join(@RequestBody MemberCreate memberCreate){
+    public ResponseEntity<String> join(@Valid  @RequestBody MemberCreate memberCreate){
         memberService.createMember(memberCreate.toEntity(), memberCreate.getPassword().toEntity());
 
         return ResponseEntity.ok().body(null);
