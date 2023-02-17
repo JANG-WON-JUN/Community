510d153acbc0c2f336cd393e3af68ed68ed374b6
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-02-18 07:20:17 +0900

    add description field

--- src/main/java/com/jwj/community/domain/enums/BoardTypes.java
+++ src/main/java/com/jwj/community/domain/enums/BoardTypes.java
@@ -1,5 +1,13 @@
 package com.jwj.community.domain.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum BoardTypes {
-    DAILY, DEV
+    DAILY("일상 게시판"),
+    DEV("개발 게시판");
+
+    private String description;
 }
