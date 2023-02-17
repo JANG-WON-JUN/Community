7256c0597823549758133ac518c016802eac223a
w32807 <w32807@nate.com>
2023-02-17 18:21:41 +0900

    add validation logic

--- src/main/java/com/jwj/community/web/controller/member/MemberController.java
+++ src/main/java/com/jwj/community/web/controller/member/MemberController.java
@@ -2,10 +2,13 @@ package com.jwj.community.web.controller.member;
 
 import com.jwj.community.domain.service.member.MemberService;
 import com.jwj.community.web.dto.member.request.MemberCreate;
+import com.jwj.community.web.validator.MemberCreateValidator;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService memberService;
+    private final MemberCreateValidator memberCreateValidator;
 
     @PostMapping("/api/join")
-    public ResponseEntity<String> join(@Valid @RequestBody MemberCreate memberCreate){
+    public ResponseEntity<String> join(@Valid @RequestBody MemberCreate memberCreate, BindingResult bindingResult) throws BindException {
+        memberCreateValidator.validate(memberCreate, bindingResult);
+
+        if(bindingResult.hasErrors()){
+            throw new BindException(bindingResult);
+        }
+
         memberService.createMember(memberCreate.toEntity(), memberCreate.getPassword().toEntity());
 
         return ResponseEntity.ok().body(null);
