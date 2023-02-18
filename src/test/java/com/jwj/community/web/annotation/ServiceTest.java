39288c9731632b10e0008d7e502ccb8a0e29edc9
w32807 <w32807@nate.com>
2023-02-18 15:11:29 +0900

    add annotation for test

---
+++ src/test/java/com/jwj/community/web/annotation/ServiceTest.java
@@ -0,0 +1,20 @@
+package com.jwj.community.web.annotation;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@SpringBootTest
+@ActiveProfiles("test") // test 시에는 test profile을 사용할 수 있도록 설정
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@Transactional
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ServiceTest {
+}
