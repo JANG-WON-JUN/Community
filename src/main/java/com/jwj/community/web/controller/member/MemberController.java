a125f1b5cf95ffc20daaccfb159e5856128321dc
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-02-24 17:12:11 +0900

    add email and nickname duplicate logic

--- src/main/java/com/jwj/community/web/controller/member/MemberController.java
+++ src/main/java/com/jwj/community/web/controller/member/MemberController.java
@@ -1,6 +1,7 @@
 package com.jwj.community.web.controller.member;
 
 import com.jwj.community.domain.service.member.MemberService;
+import com.jwj.community.web.common.result.Result;
 import com.jwj.community.web.dto.member.request.MemberCreate;
 import com.jwj.community.web.validator.MemberCreateValidator;
 import jakarta.validation.Valid;
@@ -11,7 +12,10 @@ import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import static java.lang.Boolean.TRUE;
 
 @Slf4j
 @RestController
@@ -34,4 +38,25 @@ public class MemberController {
         return ResponseEntity.ok().body(null);
     }
 
+    @PostMapping("/api/join/emailCheck")
+    public ResponseEntity<Result<Boolean>> isEmailDuplicate(@RequestParam String email) {
+        memberService.findByEmail(email);
+
+        Result<Boolean> result = Result.<Boolean>builder()
+            .data(TRUE)
+            .build();
+
+        return ResponseEntity.ok().body(result);
+    }
+
+    @PostMapping("/api/join/nicknameCheck")
+    public ResponseEntity<Result<Boolean>> isNicknameDuplicate(@RequestParam String nickname) {
+        memberService.findByNickname(nickname);
+
+        Result<Boolean> result = Result.<Boolean>builder()
+            .data(TRUE)
+            .build();
+
+        return ResponseEntity.ok().body(result);
+    }
 }
