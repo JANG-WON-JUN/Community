82a544cc4a042b83e3c9b4de8ce562bdc12cd236
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-03-01 14:23:36 +0900

    modify to be public static

--- src/main/java/com/jwj/community/web/enums/BoardSearchType.java
+++ src/main/java/com/jwj/community/web/enums/BoardSearchType.java
@@ -12,8 +12,8 @@ public enum BoardSearchType {
         // 만약 클라이언트에서 넘어온 값과 ENUM의 값을 다르게 설계 후
         // jackson 라이브러리로 mapping 시키고자 할 때 @JsonCreator를 선언
         // ex) 클라이언트에서 {"searchType" : "t"} 를 서버로 전달하면
-        // 서버에서는 enum 클래스 안에 선언된 @JsonCreator가 붙은 메소드를 사용하여
-        // 매핑을 할 수 있다.
+        // 서버에서는 enum 클래스 안에 선언된 @JsonCreator가 붙은 메소드를 사용하여 매핑을 할 수 있다.
+        // 그리고 반드시 public static으로 메소드를 선언해야 한다.
         return stringToBoardSearchType(source);
     }
 }
