f93fe4f84bf695837f55d8923f6a79c57996ae5b
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-03-30 17:25:30 +0900

    modify fetch type

--- src/main/java/com/jwj/community/domain/entity/member/auth/embedded/RoleResourcesId.java
+++ src/main/java/com/jwj/community/domain/entity/member/auth/embedded/RoleResourcesId.java
@@ -16,11 +16,11 @@ import lombok.NoArgsConstructor;
 public class RoleResourcesId {
 
     // 복합키에서는 엔티티의 PK 컬럼명이 id더라도 변수명 + id로 joincolumn에 엔티티클래스명 + id로 명시해줘야 한다.
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "role_id")
     private Role role;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "resources_id")
     private Resources resources;
 
